77132cb987608b593d2784e2f121f89187c436c5
SadiaZerin <126977609+SadiaZerin@users.noreply.github.com>
2024-04-05 01:17:35 +0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -10,11 +10,9 @@ from dotenv import load_dotenv
 from validate_email import validate_email
 
 load_dotenv()
-
-   
-def create_email_message(email,first_name,last_name):
-    
-   
+  
+def createAndSend_email_message(email,first_name,last_name):
+     
             # Create an EmailMessage object
             message = EmailMessage()
             # Set the subject of the email
@@ -30,23 +28,22 @@ def create_email_message(email,first_name,last_name):
             
             # Encode the message as bytes and encode it to a URL-safe base64 string
             data={'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
-            send_email(data)
+           
       
-      
-def send_email(message):
-    # Load credentials from token.json file
-    with open('token.json', 'r') as token_file:
-        credentials_data = token_file.read()
-
-    credentials = Credentials.from_authorized_user_info(json.loads(credentials_data))
-
-    # Build Gmail service with provided credentials
-    service = build('gmail', 'v1', credentials=credentials)
-
-    # Send email
-    try:
-        sent_message = service.users().messages().send(userId='me', body=message).execute()
-        print('Message sent successfully!')
-    except HttpError as e:
-        print(f'An error occurred while sending the email: {e}')
+             # Load credentials from token.json file
+             with open('token.json', 'r') as token_file:
+                 credentials_data = token_file.read()
+         
+             credentials = Credentials.from_authorized_user_info(json.loads(credentials_data))
+         
+             # Build Gmail service with provided credentials
+             service = build('gmail', 'v1', credentials=credentials)
+         
+             # Send email
+             try:
+                sent_message = service.users().messages().send(userId='me', body=data).execute()
+                print('Message sent successfully!')
+                return True
+             except HttpError as e:
+                print(f'An error occurred while sending the email: {e}')
 
